04c9e1e4eafc426920628c5e19fcd30f0fdd4426
devNat0 <public.dev.email.nat@gmail.com>
2026-05-21 17:49:44 +0800

    update pyo3

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-#[macro_use] extern crate rocket;
-use std::{fs, path::Path};
-use pyo3::prelude::*;
+#[macro_use]
+extern crate rocket;
+use pyo3::{ffi::c_str, prelude::*};
+use pyo3::types::IntoPyDict;
 use rocket::response::content::RawHtml;
+use std::ffi::CString;
+use std::{fs, path::Path};
 
 const APP_ROOT: &str = "python_app/";
 const PROJECT_ROOT: &str = env!("CARGO_MANIFEST_DIR");
 
 #[pyclass]
 struct LoggingStdout {
-    buffer: String
+    buffer: String,
 }
 
 #[pymethods]
@@ -22,14 +25,14 @@ fn parse(url: &str) -> String {
     let py_path = Path::new(PROJECT_ROOT)
         .join(APP_ROOT)
         .join(url.to_owned() + ".py");
-    let py_app = fs::read_to_string(py_path).unwrap();
-    Python::with_gil(|py| {
+    let code = fs::read_to_string(py_path).unwrap();
+    Python::attach(|py| {
         let obj = Bound::new(py, LoggingStdout{buffer: "".to_string()}).unwrap();
-        let sys = py.import_bound("sys").unwrap();
-        sys.setattr("stdout", obj.clone().into_py(py)).unwrap();
-        PyModule::from_code_bound(py, &py_app, "", "").unwrap();
-        let gad_rs: PyRefMut<'_, LoggingStdout> = obj.extract().unwrap();
-        gad_rs.buffer.clone()
+        let sys = py.import("sys").unwrap();
+        let _ = sys.setattr("stdout", &obj);
+        PyModule::from_code(py, CString::new(code).unwrap().as_c_str(), c"mycode.py", c"mycode").unwrap();
+        let val: PyRefMut<'_, LoggingStdout> = obj.extract().unwrap();
+        val.buffer.clone()
     })
 }
 
